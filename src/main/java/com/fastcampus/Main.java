1e8a893d3402a3f3d69866e3ca7bcf8e703454c6
silveriron <kichul7493@gmail.com>
2023-07-21 18:35:43 +0900

    Implement entry point
    정렬 기능을 메인에서 사용

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -1,7 +1,14 @@
 package com.fastcampus;
 
+import com.fastcampus.logic.BubbleSort;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort<String> bubbleSort = new BubbleSort<>();
+
+        System.out.println("[result] " + bubbleSort.sort(Arrays.asList(args)));
     }
 }
